2e329d042089486e0f015ff4aad5a4f9914f628c
ChanSil <silviochan1234@gmail.com>
2025-03-07 14:05:46 +0800

    PTSD__1stday

---
+++ src/AppUpdate.cpp
@@ -0,0 +1,39 @@
+#include "App.hpp"
+
+#include "Util/Input.hpp"
+#include "Util/Keycode.hpp"
+
+void App::Update() {
+
+    // TODO: Add your own logics to finish the tasks in README.md || Util::Input::IfExit()
+    if (Util::Input::IsKeyPressed(Util::Keycode::W) ) {
+        m_Giraffe->SetPosition({m_Giraffe->GetPosition().x,m_Giraffe->GetPosition().y+5});
+    }
+    if (Util::Input::IsKeyPressed(Util::Keycode::A) ) {
+        m_Giraffe->SetPosition({m_Giraffe->GetPosition().x-5,m_Giraffe->GetPosition().y});
+    }
+    if (Util::Input::IsKeyPressed(Util::Keycode::S) ) {
+        m_Giraffe->SetPosition({m_Giraffe->GetPosition().x,m_Giraffe->GetPosition().y-5});
+    }
+    if (Util::Input::IsKeyPressed(Util::Keycode::D) ) {
+        m_Giraffe->SetPosition({m_Giraffe->GetPosition().x+5,m_Giraffe->GetPosition().y});
+    }
+
+    /*
+     *  Do not touch the code below as they serve the purpose for validating the tasks,
+     *  rendering the frame, and exiting the game.
+    */
+
+    if (Util::Input::IsKeyPressed(Util::Keycode::ESCAPE) || Util::Input::IfExit()) {
+        m_CurrentState = State::END;
+    }
+
+    if (m_EnterDown) {
+        if (!Util::Input::IsKeyPressed(Util::Keycode::RETURN)) {
+            ValidTask();
+        }
+    }
+    m_EnterDown = Util::Input::IsKeyPressed(Util::Keycode::RETURN);
+
+    m_Root.Update();
+}
